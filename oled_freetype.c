67257b00f727bfa6236dad993bf22ebd4fbabdc3
redblue-pkt <redblue-pkt@orange.pl>
2019-06-14 03:47:31 +0200

    fix comparison between pointer and zero character constant, makes integer from pointer without a cast

--- oled_freetype.c
+++ oled_freetype.c
@@ -161,13 +161,13 @@ int lcd_print_text_up(const char* text, int color, int font_size, int align)
 	int up_x = lcd_get_xres() * LCD_UP_X;
 	int up_y = lcd_get_yres() * LCD_UP_Y;
 	int tmp;
-	if (font_size == NULL)
+	if (font_size != '\0')
 	{
-		tmp = lcd_get_xres() * LCD_UP_SIZE;
+		tmp = font_size;
 	}
 	else
 	{
-		tmp = font_size;
+		tmp = lcd_get_xres() * LCD_UP_SIZE;
 	}
 	render_lcd_text(text, up_x, up_y, 0, LCD_UP_COLOR, tmp, align);
 }
@@ -177,13 +177,13 @@ int lcd_print_text_center(const char* text, int color, int font_size, int align)
 	int center_x = lcd_get_xres() * LCD_CENTER_X;
 	int center_y = lcd_get_yres() * LCD_CENTER_Y;
 	int tmp;
-	if (font_size == NULL)
+	if (font_size != '\0')
 	{
-		tmp = lcd_get_xres() * LCD_CENTER_SIZE;
+		tmp = font_size;
 	}
 	else
 	{
-		tmp = font_size;
+		tmp = lcd_get_xres() * LCD_CENTER_SIZE;
 	}
 	render_lcd_text(text, center_x, center_y, 0, LCD_CENTER_COLOR, tmp, align);
 }
@@ -193,13 +193,13 @@ int lcd_print_text_down(const char* text, int color, int font_size, int align)
 	int down_x = lcd_get_xres() * LCD_DOWN_X;
 	int down_y = lcd_get_yres() * LCD_DOWN_Y;
 	int tmp;
-	if (font_size == NULL)
+	if (font_size != '\0')
 	{
-		tmp = lcd_get_xres() * LCD_DOWN_SIZE;
+		tmp = font_size;
 	}
 	else
 	{
-		tmp = font_size;
+		tmp = lcd_get_xres() * LCD_DOWN_SIZE;
 	}
 	render_lcd_text(text, down_x, down_y, 0, LCD_DOWN_COLOR, tmp, align);
 }
@@ -209,13 +209,13 @@ int lcd_print_text_up_different(const char* text, int color, int font_size, int 
 	int up_x_different = lcd_get_xres() * LCD_UP_X_DIFFERENT;
 	int up_y_different = lcd_get_yres() * LCD_UP_Y_DIFFERENT;
 	int tmp;
-	if (font_size == NULL)
+	if (font_size != '\0')
 	{
-		tmp = lcd_get_xres() * LCD_UP_SIZE_DIFFERENT;
+		tmp = font_size;
 	}
 	else
 	{
-		tmp = font_size;
+		tmp = lcd_get_xres() * LCD_UP_SIZE_DIFFERENT;
 	}
 	render_lcd_text(text, up_x_different, up_y_different, 0, LCD_UP_COLOR_DIFFERENT, tmp, align);
 }
@@ -225,13 +225,13 @@ int lcd_print_text_center_different(const char* text, int color, int font_size, 
 	int center_x_different = lcd_get_xres() * LCD_CENTER_X_DIFFERENT;
 	int center_y_different = lcd_get_yres() * LCD_CENTER_Y_DIFFERENT;
 	int tmp;
-	if (font_size == NULL)
+	if (font_size != '\0')
 	{
-		tmp = lcd_get_xres() * LCD_CENTER_SIZE_DIFFERENT;
+		tmp = font_size;
 	}
 	else
 	{
-		tmp = font_size;
+		tmp = lcd_get_xres() * LCD_CENTER_SIZE_DIFFERENT;
 	}
 	render_lcd_text(text, center_x_different, center_y_different, 0, LCD_CENTER_COLOR_DIFFERENT, tmp, align);
 }
@@ -241,13 +241,12 @@ int lcd_print_text_down_different(const char* text, int color, int font_size, in
 	int down_x_different = lcd_get_xres() * LCD_DOWN_X_DIFFERENT;
 	int down_y_different = lcd_get_yres() * LCD_DOWN_Y_DIFFERENT;
 	int tmp;
-	if (font_size == NULL)
-	{
-		tmp = lcd_get_xres() * LCD_DOWN_SIZE_DIFFERENT;
-	}
-	else
+	if (font_size != '\0')
 	{
 		tmp = font_size;
+	}else
+	{
+		tmp = lcd_get_xres() * LCD_DOWN_SIZE_DIFFERENT;
 	}
 	render_lcd_text(text, down_x_different, down_y_different, 0, LCD_DOWN_COLOR_DIFFERENT, tmp, align);
 }
